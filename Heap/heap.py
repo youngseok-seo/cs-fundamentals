c7d5ebc93407b77ce5eec212974bdc93e82d1bed
Young Seok Seo <young7seo@gmail.com>
2019-10-07 00:57:16 -0400

    added heap with heapify

---
+++ Heap/heap.py
@@ -0,0 +1,40 @@
+class minHeap:
+    def __init__(self):
+        self.store = []
+        self.length = len(self.store)
+
+    def addElement(self, val):
+        self.store += [val]
+        self.length += 1
+        if self.length > 1:
+            self.heapify(self.length - 1)
+
+        print(self.store)
+        return True
+
+    def heapify(self, idx):
+        print("heapify")
+        print(self.store[idx])
+        if idx % 2 == 1:
+            parent = (idx - 1) // 2
+        else:
+            parent = (idx - 2) // 2
+        print(self.store[parent])
+
+        if self.store[idx] < self.store[parent]:
+            oldParent = self.store[parent]
+            self.store[parent] = self.store[idx]
+            self.store[idx] = oldParent
+
+            if self.length > 3:
+                self.heapify(parent)
+
+
+x = minHeap()
+x.addElement(1)
+x.addElement(3)
+x.addElement(5)
+x.addElement(2)
+x.addElement(4)
+
+print(x.store)
